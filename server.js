d7509f6cf1a10d653b96d967f821ac0f6590b061
samuelbadcock15-web <samuelbadcock15@gmail.com>
2026-08-20 18:39:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const io = new Server(server, {
         origin: "*",
         methods: ["GET", "POST"]
     },
+    transports: ['websocket', 'polling'],
     pingTimeout: 60000,
     pingInterval: 25000
 });
@@ -22,19 +23,14 @@ app.get('/', (req, res) => {
 
 let waitingPlayers = [];
 const rooms = {};
-let totalOnlinePlayers = 0;
-
-function broadcastPlayerCounts() {
-    io.emit('update_counts', { totalOnline: totalOnlinePlayers, inQueue: waitingPlayers.length });
-}
 
 io.on('connection', (socket) => {
-    totalOnlinePlayers++;
-    console.log('A player connected:', socket.id);
-    broadcastPlayerCounts();
+    console.log(`--> Player connected: ${socket.id}`);
 
+    // Push player into matchmaking queue
     waitingPlayers.push(socket);
     socket.emit('waiting_for_opponent');
+    console.log(`Current queue length: ${waitingPlayers.length}`);
 
     if (waitingPlayers.length >= 2) {
         const p1 = waitingPlayers.shift();
@@ -46,65 +42,30 @@ io.on('connection', (socket) => {
 
         rooms[roomId] = { p1: p1.id, p2: p2.id };
 
-        io.to(p1.id).emit('assigned_role', { playerIndex: 1, roomId, team: 'player' });
-        io.to(p2.id).emit('assigned_role', { playerIndex: 2, roomId, team: 'enemy' });
+        io.to(p1.id).emit('assigned_role', { roomId, team: 'player' });
+        io.to(p2.id).emit('assigned_role', { roomId, team: 'enemy' });
 
-        io.to(roomId).emit('start_match');
-        console.log(`Match started in room: ${roomId} (P1: ${p1.id}, P2: ${p2.id})`);
-        broadcastPlayerCounts();
+        console.log(`=== MATCH STARTED === Room: ${roomId} | P1: ${p1.id} vs P2: ${p2.id}`);
     }
 
-    // Relay unit spawns
-    socket.on('spawn_unit', (data) => {
-        socket.to(data.roomId).emit('spawn_unit', data);
-    });
-
-    // Relay structure purchases and upgrades so both players see them
-    socket.on('buy_structure', (data) => {
-        socket.to(data.roomId).emit('buy_structure', data);
-    });
-
-    socket.on('upgrade_structure', (data) => {
-        socket.to(data.roomId).emit('upgrade_structure', data);
-    });
-
-    socket.on('toggle_gate', (data) => {
-        socket.to(data.roomId).emit('toggle_gate', data);
-    });
-
-    socket.on('player_move', (data) => {
-        socket.to(data.roomId).emit('opponent_move', data);
-    });
-
-    socket.on('fire_projectile', (data) => {
-        socket.to(data.roomId).emit('opponent_fire', data);
-    });
-
-    socket.on('destroy_unit', (data) => {
-        socket.to(data.roomId).emit('opponent_destroy', data);
-    });
-
-    socket.on('check_game_over', (data) => {
-        io.to(data.roomId).emit('trigger_game_over', data);
-    });
-
-    socket.on('request_restart', (data) => {
-        io.to(data.roomId).emit('restart_game');
-    });
+    // Relay actions between players
+    socket.on('spawn_unit', (data) => socket.to(data.roomId).emit('spawn_unit', data));
+    socket.on('buy_structure', (data) => socket.to(data.roomId).emit('buy_structure', data));
+    socket.on('upgrade_structure', (data) => socket.to(data.roomId).emit('upgrade_structure', data));
+    socket.on('toggle_gate', (data) => socket.to(data.roomId).emit('toggle_gate', data));
+    socket.on('player_move', (data) => socket.to(data.roomId).emit('opponent_move', data));
 
     socket.on('disconnect', () => {
-        totalOnlinePlayers = Math.max(0, totalOnlinePlayers - 1);
-        console.log('A player disconnected:', socket.id);
-        
+        console.log(`<-- Player disconnected: ${socket.id}`);
         waitingPlayers = waitingPlayers.filter(s => s.id !== socket.id);
 
         for (const roomId in rooms) {
             if (rooms[roomId].p1 === socket.id || rooms[roomId].p2 === socket.id) {
                 io.to(roomId).emit('opponent_disconnected');
                 delete rooms[roomId];
+                console.log(`Match ended / Room deleted: ${roomId}`);
             }
         }
-        broadcastPlayerCounts();
     });
 });
 
