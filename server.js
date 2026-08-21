873f9130faa17cfc79377b3e0deac02450c664a9
samuelbadcock15-web <samuelbadcock15@gmail.com>
2026-08-21 09:25:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,53 +8,41 @@ const io = new Server(server, { cors: { origin: '*' } });
 
 const PORT = process.env.PORT || 3000;
 
-// Serves index.html directly from the root directory
 app.use(express.static(__dirname));
 
-let players = {};
 let bluePlayer = null;
 let redPlayer = null;
 
 io.on('connection', (socket) => {
-    // 1st player is Blue, 2nd player is Red, others spectate
     if (!bluePlayer) {
         bluePlayer = socket.id;
-        players[socket.id] = { team: 'blue' };
         socket.emit('roleAssign', { team: 'blue' });
     } else if (!redPlayer) {
         redPlayer = socket.id;
-        players[socket.id] = { team: 'red' };
         socket.emit('roleAssign', { team: 'red' });
     } else {
-        players[socket.id] = { team: 'spectator' };
         socket.emit('roleAssign', { team: 'spectator' });
     }
 
-    // Relay tank position and rotation
     socket.on('playerMove', (data) => {
         socket.broadcast.emit('enemyMove', data);
     });
 
-    // Relay weapon fire
     socket.on('playerFire', (data) => {
         socket.broadcast.emit('enemyFire', data);
     });
 
-    // Relay placed cubes
     socket.on('placeBlock', (data) => {
         socket.broadcast.emit('blockPlaced', data);
     });
 
-    // Relay unit spawns
     socket.on('spawnUnit', (data) => {
         io.emit('unitSpawned', data);
     });
 
-    // Handle disconnections and free up player slots
     socket.on('disconnect', () => {
         if (socket.id === bluePlayer) bluePlayer = null;
         if (socket.id === redPlayer) redPlayer = null;
-        delete players[socket.id];
         io.emit('playerLeft', { id: socket.id });
     });
 });
